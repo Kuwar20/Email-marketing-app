1affecd9aabc2ee1c6815e285b21754715a5edb1
Kuwar20 <kuwarx1@gmail.com>
2024-11-26 11:31:40 +0530

    remove unused server and client files, including configuration and styling

--- email-marketing-app/server/src/model/Flow.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from 'mongoose';
-
-const flowSchema = new Schema({
-  nodes: Array,
-  edges: Array,
-  createdAt: { type: Date, default: Date.now },
-});
-
-export default model('Flow', flowSchema);
